1c7c05a724bbc7794a6944b474dbe5387a49ba69
yudhit08 <yudhitrenandra99@gmail.com>
2022-08-25 20:08:17 +0700

    remove persentage

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -4,7 +4,7 @@ function Score(props) {
     return (
         <div className='score-container'>
             <h3>
-                Score: {Math.floor(eval(props.right/props.number * 100))}%
+                Score: {Math.floor(eval(props.right/props.number * 100))}
             </h3>
         </div>
     );
